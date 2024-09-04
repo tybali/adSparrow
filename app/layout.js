cecec46e2d61081abadc0d3f990346534d0dc992
nimrabaig <nimrabaig28@gmail.com>
2024-09-04 19:22:36 +0500

    added gtag

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,15 @@ import "./globals.css";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import AOSProvider from "@/components/AOSProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 // import Preloader from "@/layouts/Preloader";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <script
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-P1CLG235SD"
         ></script>
@@ -28,12 +30,15 @@ export default function RootLayout({ children }) {
 
             gtag('config', 'G-P1CLG235SD');
           `}
-        </script>
-        
+          GTM-NQW6CKGZ
+        </script> */}
+
         <meta name="robots" content="all" />
         <meta name="googlebot" content="all" />
         <link rel="canonical" href="https://www.adsparrow.net/" />
       </Head>
+      <GoogleTagManager gtmId="GTM-NQW6CKGZ" />
+      <GoogleAnalytics gaId="G-P1CLG235SD" />
       <body>
         <AOSProvider>
           {/* <Preloader /> */}
